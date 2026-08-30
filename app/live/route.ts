99349f69320910f1b83925603e1757b252e5c45a
unknown <imlukadev@gmail.com>
2026-08-30 10:12:43 -0300

    hide game

--- app/live/route.ts
+++ app/live/route.ts
@@ -1,9 +1,9 @@
 import { redirect } from 'next/navigation'
 
 export async function GET() {
-redirect(`http://localhost:8000/live`)
+redirect(`https://neuhof-backend.onrender.com`)
 }
 
 export async function HEAD() {
-  redirect(`http://localhost:8000/live`)
+  redirect(`https://neuhof-backend.onrender.com`)
 }
